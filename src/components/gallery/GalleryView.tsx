ce157a28419a0864ad091b1ebebde820901ea9dd
Agnihotra Nath <agnihotranath@hotmail.com>
2025-12-14 00:51:40 +0530

    updates.

--- src/components/gallery/GalleryView.tsx
+++ src/components/gallery/GalleryView.tsx
@@ -7,11 +7,12 @@ import { Key } from "react";
 export default function GalleryView() {
   return (
     <MasonryGrid columns={2} s={{ columns: 1 }}>
-      {gallery.images.map((image: { orientation: string; src: string; alt: string | undefined; }, index: Key | null | undefined) => (
+      {gallery.images.map((image: { orientation: string; src: string; alt: string | undefined; }, index: number) => (
         <Media
           enlarge
           priority={index < 10}
           sizes="(max-width: 560px) 100vw, 50vw"
+          // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
           key={index}
           radius="m"
           aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "3 / 4"}
